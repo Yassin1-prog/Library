2c024aa79014928efa781ad7de8af03c19467c06
Yassin <132544868+Yassin1-prog@users.noreply.github.com>
2024-01-14 00:30:02 +0200

    added toggle for reading and fixed bugs with removing rows

--- script.js
+++ script.js
@@ -23,6 +23,8 @@ function deleteRow(index) {
 }
 */
 
+
+
 function display(myLibrary) {
     let tableBody = document.getElementById("booksTable").getElementsByTagName('tbody')[0];
 
@@ -33,28 +35,60 @@ function display(myLibrary) {
             // Loop through the array and add rows to the table
             myLibrary.forEach((book,index) => {
                 let row = tableBody.insertRow();
-                let cellTitle = row.insertCell(0);
-                let cellAuthor = row.insertCell(1);
-                let cellPages = row.insertCell(2);
-                let cellRead = row.insertCell(3);
-                let cellButton = row.insertCell(4);
+                if(myLibrary[index] !== 1) {
 
-                const btn = document.createElement('button');
-                btn.textContent = 'Remove';
-                btn.setAttribute("data-index", index);
+                    //let row = tableBody.insertRow();
+                    let cellTitle = row.insertCell(0);
+                    let cellAuthor = row.insertCell(1);
+                    let cellPages = row.insertCell(2);
+                    let cellRead = row.insertCell(3);
+                    let cellButton = row.insertCell(4);
 
-                cellTitle.textContent = book.title;
-                cellAuthor.textContent = book.author;
-                cellPages.textContent = book.pages;
-                cellRead.textContent = book.read;
+                    const btn = document.createElement('button');
+                    btn.textContent = 'Remove';
+                    btn.setAttribute("data-index", index);
 
-                btn.onclick = function() {
-                    let rowIndex = parseInt(btn.getAttribute("data-index"));
-                    myLibrary.splice(rowIndex, 1);
-                    tableBody.deleteRow(rowIndex);
-                    tableBody.insertRow(rowIndex);
+                    const button = document.createElement('button');
+                   // button.textContent = 'Read?';
+                    //button.classList.add("grey");
+                    if(book.read) {
+                        button.classList.add("green");
+                        button.textContent = 'YES';
+                    }
+
+                    if(!book.read) {
+                        button.classList.add("red");
+                        button.textContent = 'NO';
+                    }
+
+                    button.onclick = function() {
+                        button.classList.remove("grey");
+                        if (button.classList.contains("green")) {
+                            button.classList.remove("green");    
+                            button.classList.add("red");
+                            button.textContent = 'NO';
+                        } else {
+                            button.classList.remove("red"); 
+                            button.classList.add("green");
+                            button.textContent = 'YES';
+                        }
+                    }
+
+                    cellTitle.textContent = book.title;
+                    cellAuthor.textContent = book.author;
+                    cellPages.textContent = book.pages;
+                    //cellRead.textContent = book.read;
+
+                    btn.onclick = function() {
+                        let rowIndex = parseInt(btn.getAttribute("data-index"));
+                        //myLibrary.splice(rowIndex, 1);
+                        myLibrary[rowIndex] = 1;
+                        tableBody.deleteRow(rowIndex);
+                        tableBody.insertRow(rowIndex);
+                    }
+                    cellButton.appendChild(btn);
+                    cellRead.appendChild(button);
                 }
-                cellButton.appendChild(btn);
             });
 }
 
@@ -75,15 +109,15 @@ const dialog = document.querySelector("[data-modal]")
 const openButton = document.querySelector(".open");
 const closeButton = document.querySelector(".close");
 
-const cleanInput = document.querySelectorAll('[title], [author], [number], [read]');
+const cleanInput = document.querySelectorAll('[title], [author], [number], [checkbox]');
 
 const title = document.querySelector('[title]');
 const author = document.querySelector('[author]');
 const number = document.querySelector('[number]');
-const read = document.querySelector('[read]');
+let checkbox = document.getElementById("myCheckbox");
 
-function addToLibrary(title, author, number, read) {
-    const book = new Book(title.value, author.value, number.value, read.value);
+function addToLibrary(title, author, number, checkbox) {
+    const book = new Book(title.value, author.value, number.value, checkbox.checked);
     addBookToLibrary(book);
     display(myLibrary);
 }
@@ -97,11 +131,13 @@ openButton.addEventListener("click", () => {
 closeButton.addEventListener("click", (e) => {
     e.preventDefault();
 
-    addToLibrary(title, author, number, read);
+    addToLibrary(title, author, number, checkbox);
 
     cleanInput.forEach( input => {
          input.value = '';
     });
+
+    checkbox.checked = false;
 
     dialog.close();
 });
