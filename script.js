76daae9113f4e628e19b1d92e88df2eea69bb684
Yassin <132544868+Yassin1-prog@users.noreply.github.com>
2024-02-01 17:37:05 +0200

    added form validation

--- script.js
+++ script.js
@@ -1,18 +1,18 @@
 const myLibrary = [];
 
 function Book(title, author, pages, read) {
-    this.title = title;
-    this.author = author;
-    this.pages = pages;
-    this.read = read;
-    this.info = function() {
-        let report = '${title} by ${author}, ${pages} pages, ${read}';
-        console.log(report);
-    };
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+  this.read = read;
+  this.info = function () {
+    let report = "${title} by ${author}, ${pages} pages, ${read}";
+    console.log(report);
+  };
 }
 
 function addBookToLibrary(Book) {
-    myLibrary.push(Book);
+  myLibrary.push(Book);
 }
 /*
 function deleteRow(index) {
@@ -23,73 +23,72 @@ function deleteRow(index) {
 }
 */
 
-
-
 function display(myLibrary) {
-    let tableBody = document.getElementById("booksTable").getElementsByTagName('tbody')[0];
+  let tableBody = document
+    .getElementById("booksTable")
+    .getElementsByTagName("tbody")[0];
 
-            // Clear existing table content
-            tableBody.innerHTML = "";
-            let length = myLibrary.length;
+  // Clear existing table content
+  tableBody.innerHTML = "";
+  let length = myLibrary.length;
 
-            // Loop through the array and add rows to the table
-            myLibrary.forEach((book,index) => {
-                let row = tableBody.insertRow();
-                if(myLibrary[index] !== 1) {
+  // Loop through the array and add rows to the table
+  myLibrary.forEach((book, index) => {
+    let row = tableBody.insertRow();
+    if (myLibrary[index] !== 1) {
+      //let row = tableBody.insertRow();
+      let cellTitle = row.insertCell(0);
+      let cellAuthor = row.insertCell(1);
+      let cellPages = row.insertCell(2);
+      let cellRead = row.insertCell(3);
+      let cellButton = row.insertCell(4);
 
-                    //let row = tableBody.insertRow();
-                    let cellTitle = row.insertCell(0);
-                    let cellAuthor = row.insertCell(1);
-                    let cellPages = row.insertCell(2);
-                    let cellRead = row.insertCell(3);
-                    let cellButton = row.insertCell(4);
+      const btn = document.createElement("button");
+      btn.textContent = "Remove";
+      btn.setAttribute("data-index", index);
 
-                    const btn = document.createElement('button');
-                    btn.textContent = 'Remove';
-                    btn.setAttribute("data-index", index);
+      const button = document.createElement("button");
+      // button.textContent = 'Read?';
+      //button.classList.add("grey");
+      if (book.read) {
+        button.classList.add("green");
+        button.textContent = "YES";
+      }
 
-                    const button = document.createElement('button');
-                   // button.textContent = 'Read?';
-                    //button.classList.add("grey");
-                    if(book.read) {
-                        button.classList.add("green");
-                        button.textContent = 'YES';
-                    }
+      if (!book.read) {
+        button.classList.add("red");
+        button.textContent = "NO";
+      }
 
-                    if(!book.read) {
-                        button.classList.add("red");
-                        button.textContent = 'NO';
-                    }
+      button.onclick = function () {
+        button.classList.remove("grey");
+        if (button.classList.contains("green")) {
+          button.classList.remove("green");
+          button.classList.add("red");
+          button.textContent = "NO";
+        } else {
+          button.classList.remove("red");
+          button.classList.add("green");
+          button.textContent = "YES";
+        }
+      };
 
-                    button.onclick = function() {
-                        button.classList.remove("grey");
-                        if (button.classList.contains("green")) {
-                            button.classList.remove("green");    
-                            button.classList.add("red");
-                            button.textContent = 'NO';
-                        } else {
-                            button.classList.remove("red"); 
-                            button.classList.add("green");
-                            button.textContent = 'YES';
-                        }
-                    }
+      cellTitle.textContent = book.title;
+      cellAuthor.textContent = book.author;
+      cellPages.textContent = book.pages;
+      //cellRead.textContent = book.read;
 
-                    cellTitle.textContent = book.title;
-                    cellAuthor.textContent = book.author;
-                    cellPages.textContent = book.pages;
-                    //cellRead.textContent = book.read;
-
-                    btn.onclick = function() {
-                        let rowIndex = parseInt(btn.getAttribute("data-index"));
-                        //myLibrary.splice(rowIndex, 1);
-                        myLibrary[rowIndex] = 1;
-                        tableBody.deleteRow(rowIndex);
-                        tableBody.insertRow(rowIndex);
-                    }
-                    cellButton.appendChild(btn);
-                    cellRead.appendChild(button);
-                }
-            });
+      btn.onclick = function () {
+        let rowIndex = parseInt(btn.getAttribute("data-index"));
+        //myLibrary.splice(rowIndex, 1);
+        myLibrary[rowIndex] = 1;
+        tableBody.deleteRow(rowIndex);
+        tableBody.insertRow(rowIndex);
+      };
+      cellButton.appendChild(btn);
+      cellRead.appendChild(button);
+    }
+  });
 }
 
 // some testing
@@ -105,42 +104,57 @@ addBookToLibrary(book3);
 display(myLibrary);
 */
 
-const dialog = document.querySelector("[data-modal]")
+const dialog = document.querySelector("[data-modal]");
 const openButton = document.querySelector(".open");
 const closeButton = document.querySelector(".close");
 
-const cleanInput = document.querySelectorAll('[title], [author], [number], [checkbox]');
+const cleanInput = document.querySelectorAll(
+  "[title], [author], [number], [checkbox]"
+);
 
-const title = document.querySelector('[title]');
-const author = document.querySelector('[author]');
-const number = document.querySelector('[number]');
+const title = document.querySelector("[title]");
+const author = document.querySelector("[author]");
+const number = document.querySelector("[number]");
 let checkbox = document.getElementById("myCheckbox");
 
 function addToLibrary(title, author, number, checkbox) {
-    const book = new Book(title.value, author.value, number.value, checkbox.checked);
-    addBookToLibrary(book);
-    display(myLibrary);
+  const book = new Book(
+    title.value,
+    author.value,
+    number.value,
+    checkbox.checked
+  );
+  addBookToLibrary(book);
+  display(myLibrary);
 }
 
 //addToLibrary(title, author, number, read);
+const TITLE = document.querySelector("#title");
+const titleError = document.querySelector("#title + span.error");
 
 openButton.addEventListener("click", () => {
-    dialog.showModal();
+  dialog.showModal();
+  TITLE.addEventListener("input", (e) => {
+    if (TITLE.validity.valid) {
+      titleError.textContent = "";
+      titleError.className = "error";
+    } else {
+      titleError.textContent = "Title is required";
+      titleError.className = "error active";
+    }
+  });
 });
 
 closeButton.addEventListener("click", (e) => {
-    e.preventDefault();
+  e.preventDefault();
 
-    addToLibrary(title, author, number, checkbox);
+  addToLibrary(title, author, number, checkbox);
 
-    cleanInput.forEach( input => {
-         input.value = '';
-    });
+  cleanInput.forEach((input) => {
+    input.value = "";
+  });
 
-    checkbox.checked = false;
+  checkbox.checked = false;
 
-    dialog.close();
+  dialog.close();
 });
-
-
-
